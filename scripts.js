55294b0e96470103fe251e1f45b2c4fd759d7ee3
kthomas127 <34731501+kthomas127@users.noreply.github.com>
2026-05-12 18:19:47 -0400

    handle multiple operations and implement backspace function

--- scripts.js
+++ scripts.js
@@ -7,10 +7,14 @@ function clearDisplay(){
   display.value = "";
 }
 
+function backspace(){
+  display.value = display.value.slice(0, -1)
+}
+
 function calculate(){
   try{
     let total = '';
-    let a = null, b = null, operation = null;
+    let a = null, operation = null;
     for (const x of display.value) {
       if (!isNaN(x) && x !== ' '){
         total += x;
@@ -18,16 +22,20 @@ function calculate(){
         if (a === null){
           a = parseFloat(total);
         } else {
-          b = parseFloat(total);
+          const b = parseFloat(total);
+          if (operation === '+') a = (add(a, b));
+        else if (operation === '-') a = (subtract(a, b));
+        else if (operation === 'x') a = (multiply(a, b));
+        else if (operation === '/') a = (divide(a, b));
         }
       operation = x;
       total = '';
       }
     }
-    b = parseFloat(total);
+    const b = parseFloat(total);
     if (operation === '+') display.value = (add(a, b));
     else if (operation === '-') display.value = (subtract(a, b));
-    else if (operation === '*') display.value = (multiply(a, b));
+    else if (operation === 'x') display.value = (multiply(a, b));
     else if (operation === '/') display.value = (divide(a, b));
   }
   catch(error){
@@ -51,30 +59,3 @@ const divide = function(a, b) {
 	return a / b;
 };
 
-// const sum = function(array) {
-//   var total = 0;
-// 	for (let i = 0; i < array.length; i++) {
-//     total += array[i];
-//   }
-//   return total;
-// };
-
-// const multiply = function(array) {
-//   var result = 1;
-//   for (let i = 0; i < array.length; i++) {
-//     result *= array[i];
-//   }
-//   return result;
-// };
-
-// const power = function(base, exponent) {
-// 	return base ** exponent;
-// };
-
-// const factorial = function(fact_number) {
-//   if (fact_number <= 1) {
-//     return 1;
-//   } else {
-//     return fact_number * factorial(fact_number - 1);
-//   }
-// };
