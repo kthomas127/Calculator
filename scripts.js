794a9c470fc23c9c5c107b0660abddb1669642ef
kthomas127 <34731501+kthomas127@users.noreply.github.com>
2026-05-12 18:09:13 -0400

    implement calculate function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
+const display = document.getElementById("display");
+function addToDisplay(input){
+  display.value += input;
+}
+
+function clearDisplay(){
+  display.value = "";
+}
+
+function calculate(){
+  try{
+    let total = '';
+    let a = null, b = null, operation = null;
+    for (const x of display.value) {
+      if (!isNaN(x) && x !== ' '){
+        total += x;
+      } else if (x !== ' '){
+        if (a === null){
+          a = parseFloat(total);
+        } else {
+          b = parseFloat(total);
+        }
+      operation = x;
+      total = '';
+      }
+    }
+    b = parseFloat(total);
+    if (operation === '+') display.value = (add(a, b));
+    else if (operation === '-') display.value = (subtract(a, b));
+    else if (operation === '*') display.value = (multiply(a, b));
+    else if (operation === '/') display.value = (divide(a, b));
+  }
+  catch(error){
+    display.value = "Error";
+  }
+}
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -6,30 +43,38 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(array) {
-  var total = 0;
-	for (let i = 0; i < array.length; i++) {
-    total += array[i];
-  }
-  return total;
+const multiply = function(a, b) {
+	return a * b;
 };
 
-const multiply = function(array) {
-  var result = 1;
-  for (let i = 0; i < array.length; i++) {
-    result *= array[i];
-  }
-  return result;
+const divide = function(a, b) {
+	return a / b;
 };
 
-const power = function(base, exponent) {
-	return base ** exponent;
-};
+// const sum = function(array) {
+//   var total = 0;
+// 	for (let i = 0; i < array.length; i++) {
+//     total += array[i];
+//   }
+//   return total;
+// };
 
-const factorial = function(fact_number) {
-  if (fact_number <= 1) {
-    return 1;
-  } else {
-    return fact_number * factorial(fact_number - 1);
-  }
-};
+// const multiply = function(array) {
+//   var result = 1;
+//   for (let i = 0; i < array.length; i++) {
+//     result *= array[i];
+//   }
+//   return result;
+// };
+
+// const power = function(base, exponent) {
+// 	return base ** exponent;
+// };
+
+// const factorial = function(fact_number) {
+//   if (fact_number <= 1) {
+//     return 1;
+//   } else {
+//     return fact_number * factorial(fact_number - 1);
+//   }
+// };
